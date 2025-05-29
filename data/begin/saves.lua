33ad4779a1a4cfb182c8f58c4cc7b5ef0eb561ef
T.H.W.L <akiyixl@gmail.com>
2025-05-29 15:38:20 -0400

    kl

--- data/begin/saves.lua
+++
@@ -1,7 +0,0 @@
-function onCreatePost()
-setDataFromSave('TH2L', 'singerMoney', 0)
-setDataFromSave('TH2L', 'fighterMoney', 0)
-setDataFromSave('TH2L', 'combo', 0)
-setDataFromSave('TH2L', 'rank', 0)
-setDataFromSave('TH2L', 'doAnim', 'Song')
-end
